3ab0ab92792a4ef17366f6a01103794c72b48cea
İnan Deniz Bal <inandeniz.bal@gmail.com>
2023-06-17 18:10:53 +0300

    gelişme

--- src/components/details/ItemDetailBox.js
+++ src/components/details/ItemDetailBox.js
@@ -1,10 +1,16 @@
 import React, { useContext, useEffect, useState } from "react";
 import ItemContext from "../store/item-context";
-import classes from "./ItemDetailBox.module.css"
+import classes from "./ItemDetailBox.module.css";
 
 function ItemDetailBox() {
   const [hasTypes, setHasTypes] = useState(false);
   const ctx = useContext(ItemContext);
+  const [updatedPrice, setUpdatedPrice] = useState(0);
+
+  const addToCartHandler= () =>{
+    ctx.item.price+=updatedPrice;
+    
+  }
 
   useEffect(() => {
     if (Object.keys(ctx.item.itemTypes).length > 0) {
@@ -15,11 +21,37 @@ function ItemDetailBox() {
   return (
     <div className={classes.box}>
       {hasTypes && (
-        <div>
-          <p>DİĞER</p>
+        <div className={classes.hasNot}>
+          <div className={classes.tleft}>
+            <h1>{ctx.item.name}</h1>
+            <p>{ctx.item.price} TL</p>
+            <img src={ctx.item.address} alt="menu" />
+          </div>
+          <div className={classes.tright}>
+            {ctx.item.itemTypes.sizes.map((name, index) => (
+              <div key={name}>
+                <input
+                  type="radio"
+                  name="size"
+                  value={name}
+                  onChange={() => {
+                    const selectedPrice =
+                      ctx.item.price + ctx.item.itemTypes.prices[index];
+
+                    setUpdatedPrice(selectedPrice);
+                  }}
+                />
+                <label>{name}</label>
+              </div>
+            ))}
+          </div>
+          <button onClick={() => ctx.onAddtoCart(ctx.item)}>
+            Add to Basket
+          </button>
         </div>
       )}
-      {!hasTypes && 
+
+      {!hasTypes && (
         <div className={classes.hasNot}>
           <div className={classes.left}>
             <img src={ctx.item.address} alt="img" />
@@ -32,7 +64,7 @@ function ItemDetailBox() {
             </button>
           </div>
         </div>
-      }
+      )}
     </div>
   );
 }
