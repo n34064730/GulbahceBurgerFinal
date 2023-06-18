b97d71d5ac2b0caf484c9eb3e9d0e86aef9d635a
İnan Deniz Bal <inandeniz.bal@gmail.com>
2023-06-18 14:04:24 +0300

    bugfix

--- src/components/details/BasketContents.js
+++ src/components/details/BasketContents.js
@@ -2,14 +2,16 @@ import React from "react";
 import { useContext } from "react";
 import ItemContext from "../store/item-context";
 import BasketItem from "./BasketItem";
+import classes from "./BasketContents.module.css";
 
 function BasketContents() {
   const ctx = useContext(ItemContext);
   if (ctx.basketList.length > 0) {
     console.log(ctx.basketList[0].itemType);
     return (
-      <div>
+      <div className={classes["basket-content"]}>
         <BasketItem name="hey" itemType="DENEME" quantity="1" price="5" />
+
       </div>
     );
   }
