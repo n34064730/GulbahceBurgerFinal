2cb887beb4570e2ed1dbe8a42ae92e0938b2a8f5
İnan Deniz Bal <inandeniz.bal@gmail.com>
2023-06-18 13:53:42 +0300

    basket item
    basket item has done

--- src/components/details/BasketContents.js
+++ src/components/details/BasketContents.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import { useContext } from 'react'
-import ItemContext from "../store/item-context"
-import BasketItem from './BasketItem'
+import React from "react";
+import { useContext } from "react";
+import ItemContext from "../store/item-context";
+import BasketItem from "./BasketItem";
 
 function BasketContents() {
-    const ctx=useContext(ItemContext);
-    console.log(ctx.basketList[0].itemType)
-  return (
-    <div><BasketItem/></div>
-  )
+  const ctx = useContext(ItemContext);
+  if (ctx.basketList.length > 0) {
+    console.log(ctx.basketList[0].itemType);
+    return (
+      <div>
+        <BasketItem name="hey" itemType="DENEME" quantity="1" price="5" />
+      </div>
+    );
+  }
 }
 
-export default BasketContents
+export default BasketContents;
--- src/components/details/BasketItem.js
+++ src/components/details/BasketItem.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import classes from "./BasketItem.module.css";
 
-function BasketItem() {
+function BasketItem(props) {
   return (
-    <div>BasketItem</div>
-  )
+    <div className={classes.basketItem}>
+      <div className={classes.n}>{props.name}</div>
+      <div className={classes.i}>{props.itemType}</div>
+      <div className={classes.q}>Quantity: {props.quantity}</div>
+      <div className={classes.p}>{props.price} TL</div>
+    </div>
+  );
 }
 
-export default BasketItem
+export default BasketItem;
